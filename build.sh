c58a76c60415ec14322cbb4a2f58d55ab7debb75
Justin Johnson <justin@plausible.dev>
2022-01-05 10:14:30 -0600

    changes

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.91.0/hugo_extended_0.
 tar -xzf  hugo_extended_0.91.0_Linux-64bit.tar.gz -C /usr/local/bin
 hugo version && rm hugo_extended_0.91.0_Linux-64bit.tar.gz
 
-# install fission cli
-wget https://github.com/fission-suite/fission/releases/download/2.17.0/fission-cli-ubuntu-20.04-x86_64 -o /tmp/fission-cli.deb
-dpkg -i /tmp/fission-cli.deb
-rm /tmp/fission-cli.deb
+# # install fission cli
+# wget https://github.com/fission-suite/fission/releases/download/2.17.0/fission-cli-ubuntu-20.04-x86_64 -o /tmp/fission-cli.deb
+# dpkg -i /tmp/fission-cli.deb
+# rm /tmp/fission-cli.deb
 
 source /root/.bashrc
 
 hugo
 
-fission --version
+# fission --version
